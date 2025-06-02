9e8a36bfa3863752692d983190d4461b17142897
Suresh S <suresh.s@turbostart.co>
2025-06-02 18:56:02 +0530

    hidden top players and standings

--- src/app/page.js
+++ src/app/page.js
@@ -22,9 +22,9 @@ export default function Home() {
         <HomeTeamSection />
         <LatestUpdates />
         <IconPlayers />
-        <HomeStandingsSection />
+        {/* <HomeStandingsSection /> */}
 
-        <TopPlayers />
+        {/* <TopPlayers /> */}
         <Gallery />
 
         <Socials />
