d2983dfc2609b039a1387153e89bae4182688b92
Suresh S <suresh.s@turbostart.co>
2025-06-03 16:44:20 +0530

    updated the sponsor in home

--- src/app/page.js
+++ src/app/page.js
@@ -32,7 +32,7 @@ export default function Home() {
 
         {/* <News /> */}
 
-        {/* <Sponsorship /> */}
+        <Sponsorship />
 
         {/* <Fixtures /> */}
         {/*    <AboutT2C /> */}
--- src/components/home/Sponsorship.jsx
+++ src/components/home/Sponsorship.jsx
@@ -2,42 +2,98 @@ import Image from "next/image";
 import TitleComponent from "../common/TitleComponent";
 
 const Sponsorship = () => {
-  const newsData = [
+  const sponsorData = [
     {
-      img: "/images/home/news1.png",
-      title: "Wankhede stadium to host T20 Mumbai 2025",
+      img: "/images/home/sponsorship/meil.png",
+      title: "Powered By",
     },
     {
-      img: "/images/home/news2.png",
-      title: "Rohit Sharma Unveils the 2025 T20 Trophy",
+      img: "/images/home/sponsorship/jpi.png",
+      title: "League Sponsor",
     },
     {
-      img: "/images/home/news3.png",
-      title:
-        "Two new teams will be entering the T20 series in the upcoming series",
+      img: "/images/home/sponsorship/hoh.png",
+      title: "League Sponsor",
+    },
+    {
+      img: "/images/home/sponsorship/jkumar.png",
+      title: "Umpire Sponsor",
+    },
+    {
+      img: "/images/home/sponsorship/district.png",
+      title: "Ticketing Partner",
+    },
+    {
+      img: "/images/home/sponsorship/dream11.png",
+      title: "Fantasy Partner",
+    },
+
+    {
+      img: "/images/home/sponsorship/jiohotstar.png",
+      title: "Streaming Partner",
+    },
+
+    {
+      img: "/images/home/sponsorship/redfm.png",
+      title: "Radio Partner",
+    },
+    {
+      img: "/images/home/sponsorship/starsports.png",
+      title: "Official Bordcaster",
     },
   ];
-  return (
-    <div className="section-width section-padding">
-      <TitleComponent title={"Sponsorship"} />
 
-      <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-8  mx-auto">
-        {newsData.map((item, index) => (
-          <div
-            key={index}
-            className="relative h-[400px] bg-cover bg-no-repeat bg-center rounded-xl"
-            style={{
-              backgroundImage: `url(${item.img})`,
-            }}
-          >
-            {/* <Image src={"/images/logo/mcaLogo.png"} /> */}
-            <div className="bg-black h-24 from-black from-[50%] to-transparent to-[10%] mt-auto p-5   rounded-b-xl absolute bottom-0 w-full">
-              <h6 className="text-white text-center max-w-sm mx-auto 2xl:text-xl md:text-base text- font-normal">
-                {item.title}
-              </h6>
-            </div>
-          </div>
-        ))}
+  return (
+    <div className="bg-[url('/images/home/latestUpdateBg.png')] bg-cover bg-center bg-no-repeat">
+      <div className="section-width section-padding">
+        <TitleComponent title={"Sponsors"} />
+
+        <div className="grid gap-6 grid-cols-1 md:grid-cols-[repeat(auto-fit,minmax(260px,1fr))] bg-[#000B3D] p-4 rounded-xl w-full">
+          {sponsorData.map((item, index) => {
+            const isLast = index === sponsorData.length - 1;
+
+            return (
+              <div
+                key={index}
+                className={`rounded-xl relative text-white p-4 border border-[#4c5271] w-full ${
+                  isLast ? "md:col-span-2" : ""
+                }`}
+                style={{
+                  background:
+                    "linear-gradient(180.69deg, rgba(255, 255, 255, 0.15) -61.51%, rgba(0, 0, 0, 0.15) 91.86%)",
+                }}
+              >
+                <div className="w-full h-24 flex items-center mb-4">
+                  <div className="max-h-24 w-full flex justify-start">
+                    <img
+                      src={item.img}
+                      alt="Sponsor"
+                      className="object-contain h-20 max-w-full"
+                    />
+                  </div>
+                </div>
+
+                <div className="w-full flex flex-row">
+                  <div
+                    className="w-[20px]"
+                    style={{
+                      borderWidth: "1px",
+                      borderStyle: "solid",
+                      borderImageSource:
+                        "linear-gradient(90deg, #E07E27 -22.62%, #E93301 100%)",
+                      borderImageSlice: 1,
+                    }}
+                  ></div>
+                  <div className="flex-1 border-b-[1px] border-gray-800"></div>
+                </div>
+
+                <p className="text-lg font-medium z-20 relative mt-4">
+                  {item.title}
+                </p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
