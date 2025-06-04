28ad0d2920810ba67c694a17df2e37a1b9b5dd09
Suresh S <suresh.s@turbostart.co>
2025-06-04 15:22:22 +0530

    added equal padding for the teams logo of countdown timer

--- src/components/home/CountdownTimer.jsx
+++ src/components/home/CountdownTimer.jsx
@@ -124,7 +124,7 @@ const CountdownTimer = ({
               teamsLogoSeason3.find((team) => team.team === awayTeam)?.teamLogo
             }
             alt={awayTeam}
-            className={`w-20 ${
+            className={`w-20 p-3 ${
               awayTeam === "SoBo Mumbai Falcons" ? "bg-white" : ""
             }`}
           />
