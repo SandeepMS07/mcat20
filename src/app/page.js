2d1d7f7ad31ee849157ba3bb89fac4a97e7e5afc
judson-ts <155189292+judson-ts@users.noreply.github.com>
2025-06-13 11:50:09 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -24,7 +24,7 @@ export default async function Home() {
         <TopPlayers />
         {/* <IconPlayers /> */}
         <HomeStandingsSection />
-        {/* <TopPlayers /> */}
+      
         <Socials />
         <Gallery />
         <Sponsorship />
