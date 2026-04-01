e550b22622526d326e57dec7359cad1a3beab6fe
SurajRai16 <suraj.r@turbostart.co>
2026-04-01 14:22:24 +0530

    chore: update homepage metadata to T20 Mumbai League Season 4 2026

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import Navbar from "@/components/layout/navbar/navbar";
 import MixpanelProvider from "@/components/tracking/MixpanelProvider";
 import AppNavbarBanner from "@/components/openApp/openApp";
 export const metadata = {
-  title: "T20 Mumbai League - Season 3 2025",
+  title: "T20 Mumbai League - Season 4 2026",
   description:
     "T20 Mumbai League is a T20 cricket league in Mumbai, India. It features local teams and players, promoting cricket in the region.",
   openGraph: {
